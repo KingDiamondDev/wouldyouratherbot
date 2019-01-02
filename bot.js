6e2b4dbd0fbef36b49a1c411bc7b12d9b1b91e1f
Jiwanshu Verma <jiwanshu2003@gmail.com>
2019-01-01 19:11:30 -0500

    Upload

---
+++ bot.js
@@ -0,0 +1,45 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+var fs = require('fs');
+
+const questions = fs.readFileSync('resources/wyr_questions.txt').toString().split("\n");
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+           colorize: true
+           });
+logger.level = 'debug';
+// Initialize Discord Bot
+var bot = new Discord.Client({
+                             token: auth.token,
+                             autorun: true
+                             });
+
+bot.on('ready', function (evt) {
+       logger.info('Connected');
+       logger.info('Logged in as: ');
+       logger.info(bot.username + ' - (' + bot.id + ')');
+       });
+bot.on('message', function (user, userID, channelID, message, evt) {
+       // Our bot needs to know if it will execute a command
+       // It will listen for messages that will start with `!`
+       if (message.substring(0, 1) == '!') {
+       var args = message.substring(1).split(' ');
+       var cmd = args[0].toLowerCase();
+       
+       args = args.splice(1);
+       switch(cmd) {
+       // !wyr
+       case 'wyr':
+       var item=questions[Math.floor(Math.random()*questions.length)]
+       bot.sendMessage({
+                       to: channelID,
+                       message: item
+                       });
+       break;
+       }
+       }
+       });
+
